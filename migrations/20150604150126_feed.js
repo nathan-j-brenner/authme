5c0826c332c938c2e8623486b94277c57ddce7a2
nathan-j-brenner <nathan.j.brenner@gmail.com>
2015-06-06 09:30:39 -0700

    time stamp works.  Reading documentation is good

--- migrations/20150604150126_feed.js
+++ migrations/20150604150126_feed.js
@@ -2,7 +2,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('feed', function(table) {
     table.increments('tweet_id');
     table.string('tweet');
-    table.timestamp('posted_at');
+    table.timestamp('posted_at').defaultTo(knex.raw('now()'));
     table.string('user_id');
   });
 };
